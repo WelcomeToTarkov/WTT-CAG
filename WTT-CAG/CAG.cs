293c8fa0faf05bdda80fc81dfcad65f80a79bc8f
eukyre <130890675+eukyre@users.noreply.github.com>
2025-12-06 00:11:05 +0000

    Initial 4.0 Push

---
+++ WTT-CAG/CAG.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using WTTServerCommonLib.Models;
+using Range = SemanticVersioning.Range;
+
+namespace WTTCAG;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.wtt.cag";
+    public override string Name { get; init; } = "WTT-CAG";
+    public override string Author { get; init; } = "GrooveypenguinX, ProbablyEukyre, Tron, Wireman";
+    public override List<string>? Contributors { get; init; } = null;
+    public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
+    public override Range SptVersion { get; init; } = new("~4.0.7");
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~2.0.4") }
+    };
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; } = true;
+    public override string License { get; init; } = "CC-BY-NC-ND 4.0";
+}
+
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+public class WTTCAG(
+    WTTServerCommonLib.WTTServerCommonLib wttCommon) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+        await wttCommon.CustomLocaleService.CreateCustomLocales(assembly);
+        await wttCommon.CustomBotLoadoutService.CreateCustomBotLoadouts(assembly);
+        await wttCommon.CustomClothingService.CreateCustomClothing(assembly); 
+        wttCommon.CustomRigLayoutService.CreateRigLayouts(assembly); 
+        wttCommon.CustomSlotImageService.CreateSlotImages(assembly);
+        await Task.CompletedTask;
+    }
+}
