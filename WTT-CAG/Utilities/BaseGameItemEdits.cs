4bdec086ea2c9c8ee14224a2cd4e06b5da40e10c
eukyre <130890675+eukyre@users.noreply.github.com>
2026-01-24 17:04:57 +0000

    ACTinBLACK DTNVG implementation

---
+++ WTT-CAG/Utilities/BaseGameItemEdits.cs
@@ -0,0 +1,78 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Services;
+
+namespace WTTCAG.Utilities;
+
+[Injectable(typePriority: OnLoadOrder.PostDBModLoader + 3)]
+public class BaseGameItemEdits(
+    ISptLogger<BaseGameItemEdits> logger,
+    DatabaseService databaseService
+):IOnLoad
+{
+    public Task OnLoad()
+    {
+        EditFilters();
+        return Task.CompletedTask;
+    }
+
+    private void EditFilters()
+    {
+        var dbItems = databaseService.GetItems();
+        foreach (var (id, item) in dbItems)
+        {
+            switch (id)
+            {
+                case "5a16b8a9fcdbcb00165aa6ca":
+                    ModifySlotFilters(item, 0, 0, [
+                        "6974ce066e50d4be623b8d9b",
+                        "6974cf52ee1fb8a0683b8d9d"
+                    ]);
+                    break; //Pushing DTNVGs to TATM mount
+            }
+        }
+    }
+    
+    private void ReplaceSlotFilters(TemplateItem item, int slotIndex, int filterIndex, HashSet<MongoId> ids)
+    {
+        var slot = GetSlotAtIndex(item, slotIndex);
+        var filter = GetSlotFilterAtIndex(slot, filterIndex);
+
+        filter.Filter = ids;
+    }
+
+    private void ModifySlotFilters(TemplateItem item, int slotIndex, int filterIndex, List<MongoId> ids, bool isCartridge = false)
+    {
+        var slot = GetSlotAtIndex(item, slotIndex, isCartridge);
+        var filter = GetSlotFilterAtIndex(slot, filterIndex);
+
+        filter.Filter!.UnionWith(ids);
+    }
+    
+    private Slot GetSlotAtIndex(TemplateItem item, int index, bool isCartridge = false)
+    {
+        var slots = isCartridge ? item.Properties?.Cartridges?.ToArray() : item.Properties?.Slots?.ToArray();
+
+        if (index >= 0 && index < slots?.Length)
+        {
+            return slots[index];
+        }
+
+        throw new IndexOutOfRangeException($"Index on item slot property `{item.Name}` is out of range");
+    }
+
+    private SlotFilter GetSlotFilterAtIndex(Slot slot, int index)
+    {  
+        var slotFilter = slot.Properties?.Filters?.ToArray() ?? [];
+
+        if (index >= 0 && index < slotFilter.Length)
+        {
+            return slotFilter[index];
+        }
+
+        throw new IndexOutOfRangeException($"Index on slot property `{slot.Name}` is out of range");
+    }
+}
