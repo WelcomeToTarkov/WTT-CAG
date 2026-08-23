6a727f77b6c84ef73b9dbd5277315deac627dbb9
bushtail <evannosich@icloud.com>
2026-08-22 21:24:50 -0400

    add client mod, rename server project

---
+++ WTT-CAG.Client/CAGRedirects.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WTT_CAG.Client;
+
+internal static class CAGRedirects
+{
+    internal static readonly Dictionary<string, string> ItemUrlMap = new()
+    {
+        { "69537b77189ba5c1d5e64f4b", "https://welcometotarkov.wiki.gg/wiki/6B46_plate_carrier_(EMR)" }
+    };
+}
---
+++ WTT-CAG.Client/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+
+namespace WTT_CAG.Client;
+
+[BepInDependency("com.tyfon.wikilinks")]
+[BepInPlugin("com.wtt.cag", "WikiLinksRedirectExample", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private readonly ManualLogSource _logger = new("WTT-CAG");
+    private void Awake()
+    {
+        if (!Chainloader.PluginInfos.ContainsKey("com.tyfon.wikilinks")) { return; }
+
+        _logger.LogInfo("Tyfon's WikiLinks plugin is installed. Adding redirects...");
+        foreach (var kvp in CAGRedirects.ItemUrlMap)
+        {
+            _logger.LogDebug($"Adding redirect for {kvp.Key} to {kvp.Value}");
+            WikiLinks.RedirectRegistry.AddWikiRedirect(kvp.Key, kvp.Value);
+        }
+    }
+}
